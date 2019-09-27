40e7faf1e9a56bcf2768df4745e6fb1f36a28f79
Esther <esther191299@gmail.com>
2019-09-27 15:18:24 +0200

    Gameplay class

--- src/minigames/Game.java
+++ src/minigames/Game.java
@@ -1,5 +1,19 @@
 package minigames;
 
 public class Game {
+	
+	public game(){
+		
+	}
+	
+	private switchPlayers() {
+		
+	}
+	
+	default resetGame() {
+		
+	}
+			
 
 }
+//TODO: Klassendiagram maken
